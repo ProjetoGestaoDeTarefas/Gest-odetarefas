5bf9e73fc50f80de9c70076bf56e6ef94240ebd6
Luana <0000726260@senaimgaluno.com.br>
2024-07-07 03:41:37 -0300

    "Paginas Projetos e Tarefas"

---
+++ SistemGestao/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import Projeto from './Components/Projeto';
+import Tarefa from './Components/Tarefa';
+import Equipe from './Components/Equipe';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projeto" element={<Projeto />} />
+        <Route path="/tarefa" element={<Tarefa />} />
+        <Route path="/equipe" element={<Equipe />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+};
+
+export default App;
