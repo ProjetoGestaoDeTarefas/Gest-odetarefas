adb014fea0974065529be761d1b54f9658f92ce3
kauadoa <kauadoa@gmail.com>
2024-07-10 23:58:12 -0300

    atts routes

--- back-end/routes/userRouters.js
+++ back-end/routes/userRouters.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController');
 
-router.post('/api/user', UserController.register);
+router.post('/api/register', UserController.register);
 router.get('/api/users', UserController.list);
 router.put('/api/user/:id', UserController.edit);
 router.put('/api/user/adm/:id', UserController.admin);
