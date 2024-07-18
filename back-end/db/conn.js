5ea535664b7ace2e6dc91e9f00cff45d276a6ccd
kauadoa <kauadoa@gmail.com>
2024-07-17 22:56:32 -0300

    remoção de comentários

--- back-end/db/conn.js
+++ back-end/db/conn.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection({
     host: "127.0.0.1",       
   port: 3306,             
   user: "root",            
-  password: "admin",       
+  password: "admin",    
   database: "db_helpDesk"
 });
 
